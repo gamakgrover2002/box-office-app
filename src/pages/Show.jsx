3d73027190b5b930aacf36246842b68a58ee10da
gamak grover <gamakgroverofficial@gmail.com>
2024-11-25 16:08:12 +0530

    use loading and error

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -3,23 +3,21 @@ import { getShowData } from '../api/tvmaze';
 import { useQuery } from '@tanstack/react-query';
 
 function Show() {
-  const { showId } = useParams(); // Destructure showId from params
+  const { showId } = useParams();
 
   const { data, error, isLoading } = useQuery({
-    queryKey: ['show', showId], // Add showId to differentiate queries
+    queryKey: ['show', showId],
     queryFn: async () => {
-      return await getShowData(showId); // Return the result of the API call
+      return await getShowData(showId);
     },
     staleTime: 1000 * 60 * 5,
   });
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-
   return (
     <div>
       <h1>Show Details</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre> {/* Render data as JSON */}
+      {error && <div>{error.message}</div>}
+      {isLoading && <div>Loading...</div>}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
