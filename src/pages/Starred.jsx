68a0b74c9bfa29076dd9e9e9dc05da35fe9d4754
gamak grover <gamakgroverofficial@gmail.com>
2024-12-21 22:28:36 +0530

    get starred page data

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,6 +1,11 @@
 import { useStarredShow } from '../lib/useStarred';
+import { useQuery } from '@tanstack/react-query';
 
 function Starred() {
+  const [starredShow] = useStarredShow();
+  const { data } = useQuery({
+    queryKey: ['starredShow', starredShow],
+  });
   const [staredShow] = useStarredShow();
   return <div>{staredShow.length}</div>;
 }
