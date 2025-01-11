c6d99413848a17051ab2578fc1ea34590136cd6e
gamak grover <gamakgroverofficial@gmail.com>
2025-01-12 01:38:24 +0530

    styled components setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,16 @@ import ShowGrid from '../components/shows/showGrid';
 import ActorsGrid from '../components/actors/actorsGrid';
 import { useQuery } from '@tanstack/react-query';
 import { useSearchStr } from '../lib/useSearchStr';
+import styled from "styled-components"
+
+const Button = styled.button`
+  color: #BF4F74;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #BF4F74;
+  border-radius: 3px;
+`;
 
 function Home() {
   const [option, setOption] = useState(null);
